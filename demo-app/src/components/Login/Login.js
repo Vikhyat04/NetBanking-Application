3b4a7e87bd49b2a1cfc5bc14ba980a177b93f225
Vikhyat04 <vikhyat0407@gmail.com>
2021-06-24 07:16:15 +0530

    Log message

--- demo-app/src/components/Login/Login.js
+++ demo-app/src/components/Login/Login.js
@@ -14,7 +14,7 @@ class Login extends Component{
     constructor(props)
     {
         super(props);
-      console.log("in constructor");
+      console.log("in constructor1");
         this.state={
             email:"",
             password:"",
